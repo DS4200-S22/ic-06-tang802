d5462696f2d05cbba99a4b83a46b5eea1f3f9643
tang802 <tang.meng@husky.neu.edu>
2022-02-10 13:54:01 -0500

    ic-06

--- main.js
+++ main.js
@@ -11,4 +11,18 @@ Modified: 12/06/21
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
     console.log("Button 1 was clicked"); 
+    let newText = "Button1 was clicked!";
+    let buttonDiv = document.getElementById("button-div");
+    let printRand = document.getElementById("random_num")
+    let myRand = Math.random() * 100;
+    buttonDiv.innerHTML = newText;
+    printRand.innerHTML = myRand;
+}
+
+function button2Clicked() {
+    console.log("Button 2 was clicked");
+    let text2 = "Button 2 was clicked!";
+    let buttonDiv2 = document.getElementById("button-div2");
+    buttonDiv2.innerHTML = text2;
+
 }
